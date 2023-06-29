7da22824d3180cfee21533476cdb3d4bb67dfbb2
RazelRaz <razel16dcm@gmail.com>
2023-06-29 21:57:54 +0600

    Render Products

--- products.js
+++ products.js
@@ -1,47 +1,47 @@
-const nikeShoes = [
+const products = [
     {
       id: 1,
       name: 'Nike Air Max 90',
       price: 129.99,
-      image: 'nike_air_max_90.jpg',
+      image: 'images/jordan.webp',
       inStock: 45,
     },
     {
       id: 2,
       name: 'Nike Free RN Flyknit',
       price: 99.99,
-      image: 'nike_free_rn_flyknit.jpg',
+      image: 'images/jordan2.webp',
       inStock: 34,
     },
     {
       id: 3,
       name: 'Nike Zoom Pegasus Turbo',
       price: 159.99,
-      image: 'nike_zoom_pegasus_turbo.jpg',
+      image: 'images/jordan3.webp',
       inStock: 14,
     },
     {
       id: 4,
       name: 'Nike React Element 55',
       price: 119.99,
-      image: 'nike_react_element_55.jpg',
+      image: 'images/jordan4.webp',
       inStock: 45,
     },
     {
       id: 5,
       name: 'Nike Air Force 1',
       price: 89.99,
-      image: 'nike_air_force_1.jpg',
+      image: 'images/jordan5.webp',
       inStock: 11,
     },
     {
       id: 6,
       name: 'Nike Air Jordan 1',
       price: 169.99,
-      image: 'nike_air_jordan_1.jpg',
+      image: 'images/jordan6.webp',
       inStock: 9,
     },
   ];
   
-  console.log(nikeShoes);
+
   
--- script.js
+++ script.js
@@ -11,8 +11,10 @@
 
 let body = document.querySelector('body');
 let openShopping = document.querySelector('.shopping');
-let closeShopping = document.querySelector('.closeShopping')
+let closeShopping = document.querySelector('.closeShopping');
+let product_area_products = document.querySelector('.product_area-products');
 
+// sidebar cart open and close
 openShopping.addEventListener('click', () => {
     body.classList.add('active')
 })
@@ -20,3 +22,48 @@ openShopping.addEventListener('click', () => {
 closeShopping.addEventListener('click', () => {
     body.classList.remove('active')
 })
+
+// Render Products
+function renderProducts(){
+    products.forEach((product) => {
+        product_area_products.innerHTML += `
+            <div class="product_area-product">
+                <img src="${product.image}" alt="${product.image}">
+                <div class="product_area-product-content">
+                    <h3>${product.name}</h3>
+                    <p>$${product.price}</p>
+                    <button onclick="addToCart(${product.id})">Add To Cart</button>
+                </div>
+            </div>
+        `
+    })
+}
+renderProducts();
+
+// cart array
+let cart = [];
+
+
+// Add to Cart
+function addToCart(id) {
+    //check if the product already exists in the cart
+    if(cart.some((item) => item.id === id)) {
+        alert('product already exists in the cart')
+    } else {
+        const item = products.find((product) => product.id === id)
+        cart.push({
+            ...item,
+            numberOfUnits: 1
+        });
+        console.log(cart);
+    }
+
+    updateCart();
+}
+
+// Update Cart
+function updateCart(){
+    renderCartItems();
+    renderSubtotal();
+    
+}
